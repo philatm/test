25d524aa433b994491eb816b3e099ad44b0a1a14
philatm <philamooh@gmail.com>
2020-03-11 22:04:26 +0500

    Finish section contacts

--- js/main.js
+++ js/main.js
@@ -60,8 +60,10 @@ function initialize() {
 }
 
 // load google map
+/*
 var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
         'callback=initialize';
     document.body.appendChild(script);
+    */
